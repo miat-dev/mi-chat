699d3a854405f6a5d478ea4021eb31bde4f1caec
Md Ishtiyaque Ahmad <mda.ug19.cs@nitp.ac.in>
2023-10-18 20:29:10 +0530

    Added into remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import logo from './logo.svg';
+import React from 'react';
+
+import './App.css';
+//import 'antd/dist/antd.css';
+import LoginPage from './pages/login-page/login-page.component';
+import ChatPage from './pages/chat-page/chat-page.component';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCurrentUser } from './redux/user/user.selector';
+import RegisterPage from './pages/register-page/register-page.component';
+import RoomPage from './pages/room/room.pages';
+import DragDropPage from './pages/dragdrop/dragdrop.pages';
+
+class App extends React.Component {
+    render() {
+        const { currentUser } = this.props;
+       // console.log(currentUser)
+        return (
+            <Routes>
+             {/*  <Route path="/" element={<Join />} />
+            <Route path="/chat/:name/:room" element={<Chat />} />*/} 
+                <Route
+                    path='/'
+                    element={ (currentUser ? <Navigate to='/chat'></Navigate> : <LoginPage />)}
+                ></Route>
+                <Route
+            
+                    path='/annotation-rooms'
+                    element={ (currentUser ? <RoomPage /> : <Navigate to='/'></Navigate>)}
+                ></Route>
+                <Route
+              
+                    path='/dragdrop'
+                    element={(currentUser ? <DragDropPage /> : <Navigate to='/'></Navigate>)}
+                ></Route>
+                <Route
+                  
+                    path='/register'
+                    element={(currentUser ? <Navigate to='/chat'></Navigate> : <RegisterPage />)}
+                ></Route>
+                <Route  path='/chat' element={() => (currentUser ? <ChatPage /> : <Navigate replace to='/'></Navigate>)}></Route>
+            </Routes>
+        );
+    }
 }
 
-export default App;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+});
+
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
+/** @format */
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        {/* <React.StrictMode> */}
+        <BrowserRouter>
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
+        </BrowserRouter>
+        {/* </React.StrictMode> */}
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
